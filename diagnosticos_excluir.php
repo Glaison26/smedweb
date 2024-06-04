f2c2c37d563d2b498a6a59a25df7e726fa621b2a
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-06-04 14:54:59 -0300

    Implementação

---
+++ diagnosticos_excluir.php
@@ -0,0 +1,20 @@
+<?php // controle de acesso ao formulário
+session_start();
+if (!isset($_SESSION['newsession'])) {
+    die('Acesso não autorizado!!!');
+}
+
+if (!isset($_GET["id"])) {
+    header('location: /smedweb/diagnosticos_lista.php');
+    exit;
+}
+include("conexao.php");
+$c_id = "";
+$c_id = $_GET["id"];
+
+// Exclusão do registro
+$c_sql = "delete from diagnosticos where id=$c_id";
+
+$result = $conection->query($c_sql);
+
+header('location: /smedweb/diagnosticos_lista.php');
