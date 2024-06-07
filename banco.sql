725e19398f029c4adc2e73d250408e41d86cbc46
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-06-07 15:52:25 -0300

    banco de dados

--- banco.sql
+++ banco.sql
@@ -6,7 +6,7 @@
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
+/ !40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `agenda` (
   CONSTRAINT `FK_agenda_profissionais` FOREIGN KEY (`id_profissional`) REFERENCES `profissionais` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.agenda: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.agendaconfig
 CREATE TABLE IF NOT EXISTS `agendaconfig` (
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `agendaconfig` (
   CONSTRAINT `FK_agendaconfig_agenda` FOREIGN KEY (`id_agenda`) REFERENCES `agenda` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.agendaconfig: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.atestados
 CREATE TABLE IF NOT EXISTS `atestados` (
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `atestados` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.atestados: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.atributos_parametros_eventos
 CREATE TABLE IF NOT EXISTS `atributos_parametros_eventos` (
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `atributos_parametros_eventos` (
   CONSTRAINT `FK_atributos_parametros_eventos_parametros_eventos` FOREIGN KEY (`id_parametro`) REFERENCES `parametros_eventos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.atributos_parametros_eventos: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.componentes
 CREATE TABLE IF NOT EXISTS `componentes` (
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `componentes` (
   CONSTRAINT `FK_componentes_grupo_componentes` FOREIGN KEY (`id_grupo_componente`) REFERENCES `grupo_componentes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.componentes: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.convenios
 CREATE TABLE IF NOT EXISTS `convenios` (
@@ -114,7 +114,10 @@ CREATE TABLE IF NOT EXISTS `convenios` (
   CONSTRAINT `FK_convenios_tabela` FOREIGN KEY (`id_tabela`) REFERENCES `tabela` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.convenios: ~2 rows (aproximadamente)
+REPLACE INTO `convenios` (`id`, `id_tabela`, `nome`, `razaosocial`, `cnpj`, `inscestad`, `inscmunicipal`, `endereco`, `bairro`, `cidade`, `cep`, `uf`, `fone1`, `fone2`, `email`, `url`, `diaenvio`, `diapagamento`, `percentch`, `contato`, `obs`) VALUES
+	(1, 2, 'Unimed', 'Unimed SA', '75515874000180', '411432', '41234123', 'teste', 'tste', 'Belo Horizonte', '34505480', 'MG', '(31) 589-6369', '(31) 6958-5544', 'unimed@unimed.com.br', 'teste', '', '', '100', 'Joelson', _binary ''),
+	(2, 1, 'Casu', 'Casu SA', '00172442000115', '', '', '', '', '', '', 'AC', '(48) 2759-4679', '(31) 6995-5555', 'suporte@sabara.mg.gov.br', '', '', '', '', 'Glaison', _binary '');
 
 -- Copiando estrutura para tabela smed.diagnosticos
 CREATE TABLE IF NOT EXISTS `diagnosticos` (
@@ -122,9 +125,12 @@ CREATE TABLE IF NOT EXISTS `diagnosticos` (
   `cid` varchar(10) DEFAULT NULL,
   `descricao` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.diagnosticos: ~3 rows (aproximadamente)
+REPLACE INTO `diagnosticos` (`id`, `cid`, `descricao`) VALUES
+	(1, 'A00', 'Cólera'),
+	(6, 'G00', 'Meningite Bacteriana Não Classificada em Outra Parte');
 
 -- Copiando estrutura para tabela smed.especialidades
 CREATE TABLE IF NOT EXISTS `especialidades` (
@@ -133,7 +139,11 @@ CREATE TABLE IF NOT EXISTS `especialidades` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.especialidades: ~4 rows (aproximadamente)
+REPLACE INTO `especialidades` (`id`, `descricao`) VALUES
+	(1, 'Pediatria'),
+	(2, 'Oftalmologia'),
+	(4, 'Geriatria 1');
 
 -- Copiando estrutura para tabela smed.grupos_exames
 CREATE TABLE IF NOT EXISTS `grupos_exames` (
@@ -142,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `grupos_exames` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.grupos_exames: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.grupos_laudos
 CREATE TABLE IF NOT EXISTS `grupos_laudos` (
@@ -151,7 +161,13 @@ CREATE TABLE IF NOT EXISTS `grupos_laudos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.grupos_laudos: ~4 rows (aproximadamente)
+REPLACE INTO `grupos_laudos` (`id`, `descricao`) VALUES
+	(1, 'Exames Físicos'),
+	(2, 'Antecedentes Cirúrgicos'),
+	(4, 'PROSTATECTOMIA'),
+	(5, 'PSORÍASE'),
+	(6, 'ABDOMENOPLASTIA');
 
 -- Copiando estrutura para tabela smed.grupos_medicamentos
 CREATE TABLE IF NOT EXISTS `grupos_medicamentos` (
@@ -160,7 +176,10 @@ CREATE TABLE IF NOT EXISTS `grupos_medicamentos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.grupos_medicamentos: ~3 rows (aproximadamente)
+REPLACE INTO `grupos_medicamentos` (`id`, `descricao`) VALUES
+	(1, 'Analgésicos'),
+	(2, 'Medicamentos Dermatológicos');
 
 -- Copiando estrutura para tabela smed.grupo_componentes
 CREATE TABLE IF NOT EXISTS `grupo_componentes` (
@@ -169,7 +188,7 @@ CREATE TABLE IF NOT EXISTS `grupo_componentes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.grupo_componentes: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.historia
 CREATE TABLE IF NOT EXISTS `historia` (
@@ -182,7 +201,7 @@ CREATE TABLE IF NOT EXISTS `historia` (
   CONSTRAINT `FK_historia_pacientes` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.historia: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.indices
 CREATE TABLE IF NOT EXISTS `indices` (
@@ -192,7 +211,11 @@ CREATE TABLE IF NOT EXISTS `indices` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.indices: ~4 rows (aproximadamente)
+REPLACE INTO `indices` (`id`, `descricao`, `valor`) VALUES
+	(1, 'Real', 1),
+	(21, 'Dollar', 5.5),
+	(22, 'UTR', 30);
 
 -- Copiando estrutura para tabela smed.pacientes
 CREATE TABLE IF NOT EXISTS `pacientes` (
@@ -229,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `pacientes` (
   CONSTRAINT `FK_pacientes_profissionais` FOREIGN KEY (`id_profissional`) REFERENCES `profissionais` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.pacientes: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.parametros_eventos
 CREATE TABLE IF NOT EXISTS `parametros_eventos` (
@@ -238,17 +261,42 @@ CREATE TABLE IF NOT EXISTS `parametros_eventos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.parametros_eventos: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela smed.procedimentos
 CREATE TABLE IF NOT EXISTS `procedimentos` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `id_especialidade` int DEFAULT NULL,
   `descricao` varchar(200) NOT NULL,
-  `codigoamb` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `codigoamb` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_procedimentos_especialidades` (`id_especialidade`),
+  CONSTRAINT `FK_procedimentos_especialidades` FOREIGN KEY (`id_especialidade`) REFERENCES `especialidades` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.procedimentos: ~2 rows (aproximadamente)
+REPLACE INTO `procedimentos` (`id`, `id_especialidade`, `descricao`, `codigoamb`) VALUES
+	(12, 1, 'Ligadura Elástica do Esôfago, Estômago e Duodeno', '45658789'),
+	(13, 2, 'Laringoscopia/Traqueoscopia para Intubação Oro ou Nasotraqueal', '24010080');
+
+-- Copiando estrutura para tabela smed.procedimentos_tabelas
+CREATE TABLE IF NOT EXISTS `procedimentos_tabelas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_procedimento` int NOT NULL,
+  `id_tabela` int NOT NULL,
+  `custo` float DEFAULT NULL,
+  `valorreal` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_procedimentos_tabelas_tabela` (`id_tabela`),
+  KEY `FK_procedimentos_tabelas_procedimentos` (`id_procedimento`),
+  CONSTRAINT `FK_procedimentos_tabelas_procedimentos` FOREIGN KEY (`id_procedimento`) REFERENCES `procedimentos` (`id`),
+  CONSTRAINT `FK_procedimentos_tabelas_tabela` FOREIGN KEY (`id_tabela`) REFERENCES `tabela` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela smed.procedimentos_tabelas: ~2 rows (aproximadamente)
+REPLACE INTO `procedimentos_tabelas` (`id`, `id_procedimento`, `id_tabela`, `custo`, `valorreal`) VALUES
+	(1, 13, 2, 30, 30),
+	(2, 12, 1, 20, 20);
 
 -- Copiando estrutura para tabela smed.profissionais
 CREATE TABLE IF NOT EXISTS `profissionais` (
@@ -276,7 +324,10 @@ CREATE TABLE IF NOT EXISTS `profissionais` (
   CONSTRAINT `FK_profissionais_especialidades` FOREIGN KEY (`id_especialidade`) REFERENCES `especialidades` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.profissionais: ~2 rows (aproximadamente)
+REPLACE INTO `profissionais` (`id`, `id_especialidade`, `nome`, `endereco`, `bairro`, `cidade`, `cep`, `uf`, `fone1`, `fone2`, `email`, `url`, `identidade`, `cpf`, `datanasc`, `sexo`, `gera_agenda`, `observacao`, `crm`) VALUES
+	(1, 2, 'Glaison Queiroz', '', '', '', '', 'MG', '(48) 2759-4679', '(31) 2121-2223', 'suporte@sabara.mg.gov.br', NULL, '4565465', '69551022653', '2009-07-13', 'M', 'S', _binary 0x7465737465206465206f627365727661c3a7c3a36f, '565465'),
+	(2, 1, 'Maria de Souza Crus', 'Beco da Chica,100', 'Campeche', 'Belo Horizonte', '3450480', 'MG', '(31) 589-6369', '(31) 6995-5555', 'dasilva@gmail.com', NULL, '', '07415768051', '1997-11-04', 'M', 'S', _binary '', 'crm/mg 456456');
 
 -- Copiando estrutura para tabela smed.tabela
 CREATE TABLE IF NOT EXISTS `tabela` (
@@ -288,7 +339,10 @@ CREATE TABLE IF NOT EXISTS `tabela` (
   CONSTRAINT `FK_tabela_indices` FOREIGN KEY (`id_indice`) REFERENCES `indices` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.tabela: ~2 rows (aproximadamente)
+REPLACE INTO `tabela` (`id`, `id_indice`, `descricao`) VALUES
+	(1, 1, 'TUSS'),
+	(2, 21, 'Tabela1');
 
 -- Copiando estrutura para tabela smed.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -309,7 +363,11 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela smed.usuario: ~3 rows (aproximadamente)
+REPLACE INTO `usuario` (`id`, `nome`, `login`, `senha`, `ativo`, `prescricoes`, `pacientesdados`, `pacienteshistoria`, `agendamarcacao`, `agendageracao`, `cadastros`, `tipo`, `email`, `telefone`) VALUES
+	(1, 'Glaison Queiroz', 'Glaison', 'MTIzNDU2Nzg=', 'S', NULL, NULL, NULL, NULL, NULL, NULL, '1', 'glaison26.queiroz@gmail.com', '3136712550'),
+	(2, 'Glaison Queiroz', 'Glaison2', 'MTIzNDU2Nzg=', 'S', NULL, NULL, NULL, NULL, NULL, NULL, '1', 'suporte@sabara.mg.gov.br', '3136712550'),
+	(3, 'José da Silva', 'dasilva', 'MTIzNDU2Nzg=', 'S', NULL, NULL, NULL, NULL, NULL, NULL, '2', 'dasilva@gmail.com', '3678985412');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
