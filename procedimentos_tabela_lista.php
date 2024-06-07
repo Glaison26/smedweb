e11a66a4bc172368a9fa234a5f1ae097dd11a90e
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-06-07 14:37:05 -0300

    procedimentos x tabelas desenvolvimento

--- procedimentos_tabela_lista.php
+++ procedimentos_tabela_lista.php
@@ -7,6 +7,9 @@
 //    header('location: /raxx/voltamenunegado.php');
 //}
 include("conexao.php");
+
+// capturo o id do procedimento
+$c_id = $_GET["id"];
 ?>
 <!doctype html>
 <html lang="en">
@@ -26,6 +29,7 @@ include("conexao.php");
 
 <body>
     <script scr="https://code.jquery.com/jquery-3.3.1.js"></script>
+
     <script scr="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -39,7 +43,7 @@ include("conexao.php");
         function confirmacao(id) {
             var resposta = confirm("Deseja remover esse registro?");
             if (resposta == true) {
-                window.location.href = "/smedweb/procedimentos_excluir.php?id=" + id;
+                window.location.href = "/smedweb/procedimento_tabela_excluir.php?id=" + id;
             }
         }
     </script>
@@ -53,7 +57,7 @@ include("conexao.php");
 
     <script>
         $(document).ready(function() {
-            $('.tabprocedimentos').DataTable({
+            $('.tabproc_tabela').DataTable({
                 // 
                 "iDisplayLength": 6,
                 "order": [1, 'asc'],
@@ -76,7 +80,6 @@ include("conexao.php");
                         "sLoadingRecords": "Carregando...",
                         "sProcessing": "Processando...",
                         "sZeroRecords": "Nenhum registro encontrado",
-
                         "sLast": "Último"
                     },
                     "sSearch": "Pesquisar",
@@ -101,22 +104,19 @@ include("conexao.php");
     <script type="text/javascript">
         // Função javascript e ajax para inclusão dos dados
 
-        $(document).on('submit', '#frmaddprocedimentos', function(e) {
+        $(document).on('submit', '#frmaddindice', function(e) {
             e.preventDefault();
-            var c_procedimento = $('#addprocedimentoField').val();
-            var c_especialidade = $('#addespecialidadeField').val();
-            var c_codigoamb = $('#addcodigoambField').val();
+            var c_indice = $('#addindiceField').val();
+            var c_valor = $('#addvalorField').val();
 
-            if (c_procedimento != '') {
+            if (c_indice != '' && c_valor != '') {
 
                 $.ajax({
-                    url: "procedimentos_novo.php",
+                    url: "indices_novo.php",
                     type: "post",
                     data: {
-                        c_procedimento: c_procedimento,
-                        c_especialidade: c_especialidade,
-                        c_codigoamb: c_codigoamb
-
+                        c_indice: c_indice,
+                        c_valor: c_valor
                     },
                     success: function(data) {
                         var json = JSON.parse(data);
@@ -125,7 +125,7 @@ include("conexao.php");
                         location.reload();
                         if (status == 'true') {
 
-                            $('#novoprocedimentoModal').modal('hide');
+                            $('#novoindiceModal').modal('hide');
                             location.reload();
                         } else {
                             alert('falha ao incluir dados');
@@ -155,10 +155,8 @@ include("conexao.php");
                 console.log(data);
 
                 $('#up_idField').val(data[0]);
-                $('#up_procedimentoField').val(data[1]);
-                $('#up_especialidadeField').val(data[2]);
-                $('#up_codigoambField').val(data[3]);
-
+                $('#up_indiceField').val(data[1]);
+                $('#up_valorField').val(data[2]);
 
             });
         });
@@ -167,25 +165,21 @@ include("conexao.php");
     <script type="text/javascript">
         ~
         // Função javascript e ajax para Alteração dos dados
-        $(document).on('submit', '#frmprocedimento', function(e) {
+        $(document).on('submit', '#frmindice', function(e) {
             e.preventDefault();
             var c_id = $('#up_idField').val();
-            var c_procedimento = $('#up_procedimentoField').val();
-            var c_especialidade = $('#up_especialidadeField').val();
-            var c_codigoamb = $('#up_codigoambField').val();
+            var c_indice = $('#up_indiceField').val();
+            var c_valor = $('#up_valorField').val();
 
-
-            if (c_procedimento != '') {
+            if (c_indice != '') {
 
                 $.ajax({
-                    url: "procedimentos_editar.php",
+                    url: "indices_editar.php",
                     type: "post",
                     data: {
                         c_id: c_id,
-                        c_procedimento: c_procedimento,
-                        c_especialidade: c_especialidade,
-                        c_codigoamb: c_codigoamb
-
+                        c_indice: c_indice,
+                        c_valor: c_valor
                     },
                     success: function(data) {
                         var json = JSON.parse(data);
@@ -194,7 +188,7 @@ include("conexao.php");
                             $('#editmodal').modal('hide');
                             location.reload();
                         } else {
-                            alert('falha ao alterar dados');
+                            alert('falha ao incluir dados');
                         }
                     }
                 });
@@ -209,53 +203,63 @@ include("conexao.php");
     <div class="panel panel-primary class">
         <div class="panel-heading text-center">
             <h4>SmartMed - Sistema Médico</h4>
-            <h5>Lista de Procedimentos<h5>
+            <h5>Lista de Tabelas por Procedimento do Sistema<h5>
         </div>
     </div>
     <br>
     <div class="container -my5">
-
+        <?php
+        // faço a Leitura da tabela com sql
+        $c_sql = "SELECT procedimentos.descricao AS procedimento, tabela.descricao, procedimentos_tabelas.id, procedimentos_tabelas.custo, procedimentos_tabelas.valorreal 
+                           FROM procedimentos_tabelas
+                           JOIN procedimentos ON procedimentos_tabelas.id_procedimento=procedimentos.id
+                           JOIN tabela ON procedimentos_tabelas.id_tabela=tabela.id WHERE procedimentos_tabelas.id_procedimento='$c_id'";
+        $result = $conection->query($c_sql);
+        // verifico se a query foi correto
+        if (!$result) {
+            die("Erro ao Executar Sql!!" . $conection->connect_error);
+        }
+        $c_linhaproc = $result->fetch_assoc();
+        ?>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#novoprocedimentoModal"><span class="glyphicon glyphicon-plus"></span>
-            Novo
+        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#novoindiceModal"><span class="glyphicon glyphicon-plus"></span>
+            Incluir
         </button>
+        <a class="btn btn-secondary btn-sm" href="/smedweb/procedimentos_lista.php"><span class="glyphicon glyphicon-arrow-left"></span> Voltar</a>
         
-        <a class="btn btn-secondary btn-sm" href="/smedweb/menu.php"><span class="glyphicon glyphicon-arrow-left"></span> Voltar</a>
+       
         <hr>
-        <table class="table display table-bordered tabprocedimentos">
+        <div class='alert alert-info' role='alert'>
+            <h5>Procedimento :<?php echo " ".$c_linhaproc['procedimento']?>  </h5>
+        </div>
+        <table class="table display table-bordered tabproc_tabela">
             <thead class="thead">
                 <tr class="info">
-                    <th scope="col">No.</th>
-                    <th scope="col">Procedimento</th>
-                    <th scope="col">Especialidade</th>
-                    <th scope="col">Código AMB</th>
-                    <th scope="col">Ações</th>
+                    <th scope="col" width="5%">No.</th>
+                    <th scope="col" width="20%">Tabela</th>
+                    <th scope="col" width="10%">Custo</th>
+                    <th scope="col" width="10%">Valor em R$</th>
+                    <th scope="col" width="10%">Ações</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-
-                // faço a Leitura da tabela com sql
-                $c_sql = "SELECT procedimentos.id, procedimentos.descricao, procedimentos.codigoamb, especialidades.descricao AS especialidade FROM procedimentos
-                JOIN especialidades ON procedimentos.id_especialidade=especialidades.id";
+                $fmt = new NumberFormatter('de_DE', NumberFormatter::CURRENCY);
                 $result = $conection->query($c_sql);
-                // verifico se a query foi correto
-                if (!$result) {
-                    die("Erro ao Executar Sql!!" . $conection->connect_error);
-                }
+
                 // insiro os registro do banco de dados na tabela 
                 while ($c_linha = $result->fetch_assoc()) {
-
+                    $n_valor = 'R$ ' . $fmt->formatCurrency($c_linha['valorreal'], "   ") . "\n";
+                    //$n_valor=$c_linha['valor'];
                     echo "
                     <tr>
                     <td>$c_linha[id]</td>
                     <td>$c_linha[descricao]</td>
-                    <td>$c_linha[especialidade]</td>
-                    <td>$c_linha[codigoamb]</td>
+                    <td>$c_linha[custo]</td>
+                    <td>$n_valor</td>
                     <td>
-                    <a class='btn btn-warning btn-sm' title='Procedimentos X Tabelas' href='/smedweb/procedimentos_tabela_lista.php?id=$c_linha[id]'><span class='glyphicon glyphicon-usd'></span></a>
-                    <button class='btn btn-info btn-sm editbtn' data-toggle=modal' title='Editar Procedimento'><span class='glyphicon glyphicon-pencil'></span></button>
-                    <a class='btn btn-danger btn-sm' title='Excluir Procedimento' href='javascript:func()'onclick='confirmacao($c_linha[id])'><span class='glyphicon glyphicon-trash'></span></a>
+                    <button class='btn btn-info btn-sm editbtn' data-toggle=modal' title='Editar Indice'><span class='glyphicon glyphicon-pencil'></span></button>
+                    <a class='btn btn-danger btn-sm' title='Excluir Indice' href='javascript:func()'onclick='confirmacao($c_linha[id])'><span class='glyphicon glyphicon-trash'></span></a>
                     </td>
 
                     </tr>
@@ -268,59 +272,40 @@ include("conexao.php");
 
 
     <!-- janela Modal para inclusão de registro -->
-    <div class="modal fade" id="novoprocedimentoModal" tabindex="-1" role="dialog" aria-labelledby="novoprocedimentoModal" aria-hidden="true">
+    <div class="modal fade" id="novoindiceModal" tabindex="-1" role="dialog" aria-labelledby="novaindiceModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel">Dados de Novo Procedimento</h4>
+                    <h4 class="modal-title" id="exampleModalLabel">Dados de Novo indice</h4>
                 </div>
                 <div class="modal-body">
                     <div class='alert alert-warning' role='alert'>
                         <h5>Campos com (*) são obrigatórios</h5>
                     </div>
-                    <form id="frmaddprocedimentos" action="">
+                    <form id="frmaddindice" action="">
                         <div class="mb-3 row">
-                            <label for="addprocedimentoField" class="col-md-3 form-label">Procedimento (*)</label>
-                            <div class="col-md-9">
-                                <input type="text" class="form-control" id="addprocedimentoField" name="addprocedimentoField">
+                            <label for="addindiceField" class="col-md-3 form-label">Indice(*)</label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control" id="addindiceField" name="addindiceField">
                             </div>
                         </div>
+
                         <div class="mb-3 row">
-
-                            <label class="col-md-3 form-label">Especialidade</label>
-                            <div class="col-sm-7">
-                                <select class="form-control form-control-lg" id="addespecialidadeField" name="addespecialidadeField">
-                                    <?php
-                                    $c_sql = "SELECT especialidades.id, especialidades.descricao FROM especialidades ORDER BY especialidades.descricao";
-                                    $result = $conection->query($c_sql);
-
-                                    // insiro os registro do banco de dados na tabela 
-                                    while ($c_linha = $result->fetch_assoc()) {
-                                        echo
-                                        "<option>$c_linha[descricao]</option>";
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-
-                        </div>
-                        <div class="mb-3 row">
-                            <label for="addcodigoambField" class="col-md-3 form-label">Código AMB</label>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control" id="addcodigoambField" name="addcodigoambField">
+                            <label for="addvalorField" class="col-md-3 form-label">Valor (*)</label>
+                            <div class="col-md-3">
+                                <input type="text" class="form-control" id="addvalorField" name="addvalorField">
                             </div>
                         </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal"><span class='glyphicon glyphicon-remove'></span> Fechar</button>
+
+                        </div>
+                    </form>
                 </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><span class='glyphicon glyphicon-remove'></span> Fechar</button>
 
-                </div>
-                </form>
             </div>
-
         </div>
-    </div>
     </div>
 
 
@@ -329,45 +314,26 @@ include("conexao.php");
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel">Editar dados do Grupo</h4>
+                    <h4 class="modal-title" id="exampleModalLabel">Editar dados do Indice</h4>
                 </div>
                 <div class="modal-body">
                     <div class='alert alert-warning' role='alert'>
                         <h5>Campos com (*) são obrigatórios</h5>
                     </div>
-                    <form id="frmprocedimento" method="POST" action="">
+                    <form id="frmindice" method="POST" action="">
                         <input type="hidden" id="up_idField" name="up_idField">
                         <div class="mb-3 row">
-                            <label for="up_procedimentoField" class="col-md-3 form-label">Procedimento (*)</label>
+                            <label for="up_indiceField" class="col-md-3 form-label">Indice (*)</label>
                             <div class="col-md-9">
-                                <input type="text" class="form-control" id="up_procedimentoField" name="up_procedimentoField">
+                                <input type="text" class="form-control" id="up_indiceField" name="up_indiceField">
                             </div>
                         </div>
                         <div class="mb-3 row">
-                            <label class="col-md-3 form-label">Especialidade</label>
-                            <div class="col-sm-7">
-                                <select class="form-control form-control-lg" id="up_especialidadeField" name="up_especialidadeField">
-                                    <?php
-                                    $c_sql = "SELECT especialidades.id, especialidades.descricao FROM especialidades ORDER BY especialidades.descricao";
-                                    $result = $conection->query($c_sql);
-
-                                    // insiro os registro do banco de dados na tabela 
-                                    while ($c_linha = $result->fetch_assoc()) {
-                                        echo
-                                        "<option>$c_linha[descricao]</option>";
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-
-                        </div>
-                        <div class="mb-3 row">
-                            <label for="up_codigoambField" class="col-md-3 form-label">Código AMB</label>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control" id="up_codigoambField" name="up_codigoambField">
+                            <label for="addvalorField" class="col-md-3 form-label">Valor (*)</label>
+                            <div class="col-md-3">
+                                <input type="text" class="form-control" id="up_valorField" name="up_valorField">
                             </div>
                         </div>
-
                         <div class="modal-footer">
                             <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
                             <button type="button" class="btn btn-secondary" data-dismiss="modal"><span class='glyphicon glyphicon-remove'></span> Fechar</button>
