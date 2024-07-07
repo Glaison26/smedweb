7b0ba0abcad4987ecd91f79d608042bcb0eb19a9
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-07-07 16:53:07 -0300

    develop

--- historia.php
+++ historia.php
@@ -2,16 +2,42 @@
 include_once "lib_gop.php";
 include("conexao.php"); // conexão de banco de dados
 $c_id = $_GET["id"]; // pego a id do paciente
-// sql para pegar dados do paciente selecionado
-$c_sql = "select pacientes.id, pacientes.nome from pacientes where pacientes.id='$c_id'";
-$result = $conection->query($c_sql);
-// verifico se a query foi correto
-if (!$result) {
-    die("Erro ao Executar Sql!!" . $conection->connect_error);
-}
+$c_historia = "";
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-// insiro os registro do banco de dados na tabela 
-$c_linha = $result->fetch_assoc();
+    // sql para pegar dados do paciente selecionado
+    $c_sql = "select pacientes.id, pacientes.nome from pacientes where pacientes.id='$c_id'";
+    $result = $conection->query($c_sql);
+    // verifico se a query foi correto
+    if (!$result) {
+        die("Erro ao Executar Sql!!" . $conection->connect_error);
+    }
+    $c_linha = $result->fetch_assoc();
+    // Verifico numero de registros na historia para ver ser existe historia
+    $c_sql = "select COUNT(*) AS qtd FROM historia where historia.id_paciente='$c_id'";
+    $result = $conection->query($c_sql);
+    $c_linha_qtd = $result->fetch_assoc();
+    if ($c_linha_qtd['qtd'] == 0) {
+        $c_sql = "insert into historia (id_paciente,historia) value ('$c_id','$c_historia')";
+        $result = $conection->query($c_sql);
+    } else {
+        $c_sql = "select historia.historia from historia where historia.id_paciente='$c_id'";
+        $result = $conection->query($c_sql);
+        $c_linha_historia = $result->fetch_assoc();
+        $c_historia = $c_linha_historia['historia'];
+    }
+}
+// gravo dados da historia
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $c_historia = $_POST['historia'];
+    $c_sql = "update historia set historia='$c_historia' where id_paciente='$c_id'";
+    $result = $conection->query($c_sql);
+    header('location: /smedweb/pacientes_lista.php');
+}
+// sair da historia
+
+
+
 
 ?>
 
@@ -47,29 +73,32 @@ $c_linha = $result->fetch_assoc();
         </div>
     </div>
     <div class="container -my5">
-        <div style="padding-bottom:20px;">
-            <div class="row mb-3">
-                <div class="offset-sm-0 col-sm-3">
-                    <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
-                    <a class='btn btn-danger' href='/smedweb/pacientes_lista.php'><span class='glyphicon glyphicon-remove'></span> Voltar</a>
+        <form method="post" class="form-horizontal">
+            <div style="padding-bottom:20px;">
+
+                <div class="row mb-3">
+                    <div class="offset-sm-0 col-sm-3">
+                        <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
+                        <a class='btn btn-danger' href='/smedweb/pacientes_lista.php'><span class='glyphicon glyphicon-remove'></span> Voltar</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="panel panel-success">
-            <div class="panel-heading">Identificação do Paciente:<?php echo '' . $c_linha['nome']; ?></div>
-            <div class="panel-body">
+            <div class="panel panel-success">
+                <div class="panel-heading">Identificação do Paciente:<?php echo '' . $c_linha['nome']; ?></div>
+                <div class="panel-body">
 
-                <div style="padding-top:20px;">
+                    <div style="padding-top:20px;">
 
-                    <div class="form-group">
-                        <label class="col-sm-5 col-form-label">História Clinica</label>
-                        <div class="col-sm-12">
-                            <textarea class="form-control" id="obs" name="obs" rows="25"></textarea>
+                        <div class="form-group">
+                            <label class="col-sm-5 col-form-label">História Clinica</label>
+                            <div class="col-sm-12">
+                                <textarea class="form-control" id="historia" name="historia" rows="25"><?php echo $c_historia ?></textarea>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 
 </body>
