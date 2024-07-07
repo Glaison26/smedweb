4bc3cd16cd09a501da2e2b5408230f553a46911b
Glaison Queiroz <glaison26.queiroz@gmail.com>
2024-07-07 17:16:03 -0300

    develop

--- historia.php
+++ historia.php
@@ -84,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </div>
             </div>
             <div class="panel panel-success">
-                <div class="panel-heading">Identificação do Paciente:<?php echo '' . $c_linha['nome']; ?></div>
+                <div class="panel-heading"><h4>Identificação do Paciente:<?php echo '' . $c_linha['nome']; ?></h4></div>
                 <div class="panel-body">
 
                     <div style="padding-top:20px;">
