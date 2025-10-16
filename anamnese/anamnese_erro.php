ab91d0d2bf919e5b107975f95835bc69e9d14a16
Glaison Queiroz <glaison26.queiroz@gmail.com>
2025-10-16 00:45:25 -0300

    dev

--- anamnese/anamnese_erro.php
+++ anamnese/anamnese_erro.php
@@ -9,14 +9,11 @@ include_once("../links.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-
-
 <body>
     <div class="panel panel-primary class">
         <div class="panel-heading text-center">
             <h4>SmartMed - Sistema Médico</h4>
-            <h5>Erro de Entrada de Dados<h5>
+            <h5>Erro de Entrada de Dados da Anamnese<h5>
         </div>
     </div>
 
@@ -26,7 +23,7 @@ include_once("../links.php");
                 <h5><strong>Erro nas informações :<?php echo $_SESSION['msg_erro'] ?> </strong></h5>
             </div>
             <div class="alert alert-success">
-                <h5><strong>Use o botão de Navegação para voltar ao formulário e corrigir a informação</strong></h5>
+                <h5><strong>Use o botão de Navegação para voltar ao formulário e corrigir o erro informado acima</strong></h5>
             </div>
         </div>
     </div>
