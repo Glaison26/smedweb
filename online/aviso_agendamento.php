9aedb195528e648ef9a8577f82c345b3568e2606
Glaison <Glaison26.queiroz@gmail.com>
2025-11-07 16:30:15 -0300

    dev

---
+++ online/aviso_agendamento.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- aviso de agendamento existente -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">
+                    <h4 class="alert-heading">Aviso!</h4>
+                    <p>Você já possui um agendamento ativo. Não é possível realizar um novo agendamento enquanto houver um agendamento pendente.</p>
+                    <hr>
+                    <p class="mb-0">Por favor, entre em contato com a clínica para mais informações.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
--- online/processa_agendamento.php
+++ online/processa_agendamento.php
@@ -2,6 +2,14 @@
 // pego data e medico do formulário
 session_start();
 require_once '../conexao.php';
+// verifico através de sql se existem agendamento com a id do paciente e data igual ou superior a data atual
+$c_sql_verifica = "SELECT * FROM agenda WHERE matricula='{$_SESSION['userId']}' AND data >= CURDATE() AND (nome IS NOT null and nome <> '')";
+$result_verifica = $conection->query($c_sql_verifica);
+if ($result_verifica->num_rows > 0) {
+    // já existe agendamento, redireciono para página de aviso
+    header('Location: aviso_agendamento.php');
+    exit;
+}
 $medico_id = $_POST['medico'];
 // sql para pegar a id do médico na tabela profissionais
 $c_sql_medico = "SELECT id FROM profissionais WHERE id='$medico_id'";
