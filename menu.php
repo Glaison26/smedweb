fd3f6f1676008b3ae68eb645b29267ecb95d05c0
Glaison Queiroz <glaison26.queiroz@gmail.com>
2026-01-13 21:38:42 -0300

    dev

--- menu.php
+++ menu.php
@@ -344,13 +344,16 @@ if (($c_linha['tipo'] == '1')) {
 					</nav>
 				</div>
 			</div>
-
+<!-- submenu com as opções mais utilizadas -->
+			<hr>
+			<!-- barra de ações rápidas -->
+			 
 			<div class="container-fluid px-2">
 				<div class="user-actions">
 					<!-- Botões de ação do usuário -->
-					<a class='btn btn-success  btn-lg' href=<?php echo $op_paciente; ?>><img src="\smedweb\images\paciente.png" alt="20" width="20" height="20"></span> Ficha Clinica do Paciente</a>
-					<a class='btn btn-primary  btn-lg' href=<?php echo $op_prescricao; ?>><img src="\smedweb\images\atestado.png" alt="20" width="20" height="20"></span> Emitir Prescrições</a>
-					<a class='btn btn-info  btn-lg' href=<?php echo $op_agenda; ?>><img src="\smedweb\images\agenda.png" alt="20" width="20" height="20"></span> Marcação de Consultas</a>
+					<a class='btn btn-primary  btn-lg' href=<?php echo $op_paciente; ?>><img src="\smedweb\images\paciente.png" alt="20" width="20" height="20"></span> Ficha Clinica do Paciente</a>
+					<a class='btn btn-primary  btn-lg' href=<?php echo $op_prescricao; ?>><img src="\smedweb\images\atestado2.png" alt="20" width="20" height="20"></span> Emitir Prescrições</a>
+					<a class='btn btn-primary  btn-lg' href=<?php echo $op_agenda; ?>><img src="\smedweb\images\agenda.png" alt="20" width="20" height="20"></span> Marcação de Consultas</a>
 				</div>
 			</div>
 			<hr>
